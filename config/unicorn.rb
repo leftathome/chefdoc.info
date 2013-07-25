2c7bda8b9d86b12502e59c7ec1b77a924b602482
Steven Wagner <leftathome@gmail.com>
2013-07-25 13:56:14 -0700

    Don't know what I was thinking.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_dir = File.expand_path(File.dirname(__FILE__)+"/../")
 worker_processes 3
 working_directory app_dir
 #listen "#{app_dir}/tmp/sock/unicorn.sock"
-listen 80, :tcp_nopush => true
+listen 8080, :tcp_nopush => true
 user 'ec2-user'
 pid "#{app_dir}/tmp/pids/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.log"
