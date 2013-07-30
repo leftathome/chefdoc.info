1b11aee17791f7882b495d46c60ddb50ff8f3c36
Steven Wagner <leftathome@gmail.com>
2013-07-30 09:02:43 -0700

    How about some wildcards?

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -219,7 +219,7 @@ module YARD
       # we use the yard-chef plugin on cookbooks.
       def generate_yardoc
         `cd #{source_path} &&
-          #{YARD::ROOT}/../bin/yardoc --plugin yard-chef -n -q &&
+          #{YARD::ROOT}/../bin/yardoc --plugin yard-chef -n -q **/*.rb &&
           touch .yardoc/complete`
       end
   end
